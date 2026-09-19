fb6131244e0841e101ff295ea958b39ebe286257
xlBesh <bsooora@gmail.com>
2026-09-19 16:25:14 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 /*
   Connect Games Service Worker
-  Version: v1.3
+  Version: v1.4
 */
 
-const CACHE_NAME = "connect-games-v1.3";
+const CACHE_NAME = "connect-games-v1.4";
 
 const FILES_TO_CACHE = [
   "./",
